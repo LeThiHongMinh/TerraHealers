ef78ef24b1a6002ece288dcfb59dc0f3aa7d0a19
Thing1Thing2 <testrunmymails@gmail.com>
2024-01-30 21:24:06 +0800

    add sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
-import {ColorModeContext, useMode} from "./theme";
+import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
 
 const App = () => {
-const [theme, colorMode] = useMode();
+  const [theme, colorMode] = useMode();
 
-  return ( <ColorModeContext.Provider  value={colorMode}>
-<ThemeProvider theme={theme}>
-<CssBaseline />
-    <div className='app'> 
-<main className="content">
-</main>
-</div>
-</ThemeProvider>
-</ColorModeContext.Provider>  
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className='app'>
+        <Sidebar />
+        <main className="content">
+          <Topbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
 
   )
 }
