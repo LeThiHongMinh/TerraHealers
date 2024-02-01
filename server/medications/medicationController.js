dd51fae98848ae42afb0e4beceb46594e8c3e3c5
izzahaj <izzah.jalil02@gmail.com>
2024-02-01 10:36:11 +0800

    temp

--- server/medications/medicationController.js
+++ server/medications/medicationController.js
@@ -1,8 +1,11 @@
 import medicationService from "./medicationService";
 
 async function getMedicationById(req, res) {
+  const id = req.params.id;
+
   try {
-    const data = await medicationService.getMedicationById();
+    const data = await medicationService.getMedicationById(id);
+    res.status(200).json(data);
   } catch (err) {
 
   }
--- server/medications/medicationService.js
+++ server/medications/medicationService.js
@@ -1,8 +1,7 @@
 import { db } from "../config/db";
-import { Op, literal } from "sequelize";
+import { literal } from "sequelize";
 
 const Medication = db.medications;
-const Batch = db.batches;
 
 async function createMedication(medicationData) {
   const medication = await Medication.create(medicationData);
@@ -30,17 +29,11 @@ async function deleteMedicationById(medicationId) {
   });
 }
 
-// find all by name and filter criteria, with sorting option, and additionally counting quantity from batches
-async function searchAndPaginateMedications(searchQuery, searchBy, sortBy = "name", order = "ASC", limit = 10, page = 1) {
+async function getPaginatedMedications(limit = 10, page = 1) {
   // validate params, actual query can go into repository
   const offset = (page - 1) * limit;
   const medications = await Medication.findAll({
-    where: {
-      [searchBy]: {
-        [Op.iLike]: `%${searchQuery}%`
-      }
-    },
-    order: [[sortBy, order]],
+    order: [[sortBy, 'ASC']],
     limit,
     offset,
     attributes: [
@@ -56,4 +49,4 @@ async function searchAndPaginateMedications(searchQuery, searchBy, sortBy = "nam
   return medications;
 };
 
-export { createMedication, getMedicationById, updateMedicationById, deleteMedicationById, searchAndPaginateMedications };
+export { createMedication, getMedicationById, updateMedicationById, deleteMedicationById, getPaginatedMedications };
